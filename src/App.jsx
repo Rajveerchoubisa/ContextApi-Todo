c39a98618ea772c3e261da2b2fbb57f859730cf9
Rajveer Choubisa <ranveer0596@gmail.com>
2025-01-30 11:11:18 +0530

    context and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { Todoprovider } from './contexts/TodoContext'
+import TodoForm from '../src/components/TodoForm.jsx'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos,setTodos] = useState([])
@@ -13,18 +15,47 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem("todos"))
+
+        if(todos && todos.length > 0){
+          setTodos(todos)
+        }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+  
+
+
+
   return (
     <Todoprovider value={{todos,addTodo, updateTodo, deleteTodo, toggleComplete}}>
    <div className='bg-[#172842] min-h-screen py-8'>
     <div className='w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white'>
       <h1 className='text-2xl font-bold text-center mb-8 mt-2'>
-        Manage your todos
+        MANAGE YOUR TODO
       </h1>
       <div className='mb-4'>
         {/*todo form goes here */}
+        <TodoForm />
       </div>
       <div className='flex flex-wrap gap-y-3'>
         {/* LOop and add item  */}
+        {todos.map((todo) => (
+          <div className='w-full' key={todo.id}>
+            <TodoItem todo={todo} />
+          </div>
+        ))}
       </div>
     </div>
    </div>
